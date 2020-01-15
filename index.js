50a4c1478350adb9afe5b94cc0adafe9c7bbac90
marielapg <mpgonzales77@gmail.com>
2020-01-14 22:12:31 -0800

    node api day 2 guided

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const server = require('./api/server.js');
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
+
+
+// create a new server.js file somewhere
+// move everything but the serve.listen() out of index
+// require the server from index.js
+// module.exports the server from server.js
+// update the path to accommodate the changes (hubs-model.js)
